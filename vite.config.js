5df6660659fbfb255bd24f8feffc6d92fc7e11c9
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-10-19 01:13:42 -0700

    Fix : Redirects

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Set the base URL for the app
+  build: {
+    // Adjust the output directory if needed
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // Ensure the server responds with index.html for all routes
+  },
+});
