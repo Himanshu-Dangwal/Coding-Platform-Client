6a9faf9d32da382a8d7fdd99beb1a1e5d78209f3
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-02-24 10:02:19 -0800

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = ({ isLoggedIn, handleLogout, darkMode, toggleDarkMode }) => {
     return (
         <nav className={`navbar navbar-expand-lg fixed-top ${darkMode ? 'navbar-dark bg-dark' : 'navbar-light bg-light'}`}>
             <div className="container">
-                <Link className="navbar-brand" to="/">CodeBuddy</Link>
+                <Link className="navbar-brand" to="/" onClick={handleLinkClick}>CodeBuddy</Link>
                 <button
                     ref={navbarToggle}
                     className="navbar-toggler"
